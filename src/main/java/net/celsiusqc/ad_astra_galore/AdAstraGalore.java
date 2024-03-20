96c1c46fa33e62aaa7cc3fbc9a7b62dace0281d9
celsiusqc <63996164+celsiusqc@users.noreply.github.com>
2024-03-20 12:33:03 +0100

    Instance Setup

---
+++ src/main/java/net/celsiusqc/ad_astra_galore/AdAstraGalore.java
@@ -0,0 +1,12 @@
+package net.celsiusqc.ad_astra_galore;
+
+import net.minecraftforge.fml.common.Mod;
+
+// The value here should match an entry in the META-INF/mods.toml file
+@Mod(net.celsiusqc.ad_astra_galore.AdAstraGalore.MOD_ID)
+public class AdAstraGalore {
+    public static final String MOD_ID = "ad_astra_galore";
+
+
+    // You can use EventBusSubscriber to automatically register all static methods in the class annotated with @SubscribeEvent
+}
